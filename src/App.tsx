bcd64169955251adc5f3a0ab0aeca3216d41a879
miranda <161040125+mmfforgithub@users.noreply.github.com>
2024-12-06 10:35:22 -0300

    06/13

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import{Header} from "./components/Header"
+
+import{Footer} from "./components/Footer"
+export function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello World</h1>
+      <Footer />
+    </>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return(
+        <header>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/">Lista</a>
+            </nav>
+        </header>
+    )
+}
